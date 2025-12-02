b572516ff812cd2c9faf2b736745354d8091be76
Chanchal Raj <rajchanchalkhatri@gmail.com>
2025-12-02 23:42:17 +0400

    Int and Generic Stack

---
+++ 100 Days of Learning/Day 14/Generic Stack.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+struct IntStack {
+    private var items: [Int] = []
+    
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Int {
+        items.removeLast()
+    }
+}
+
+var myIntStack = IntStack()
+myIntStack.push(5)
+myIntStack.push(6)
+myIntStack.push(8)
+myIntStack.push(9)
+
+print("Popping: \(myIntStack.pop())")
+print("Popping: \(myIntStack.pop())")
+print("Popping: \(myIntStack.pop())")
+print("Popping: \(myIntStack.pop())")
+
+struct GenericStack <Element>{
+    private var items: [Element] = []
+    
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Element {
+        items.removeLast()
+    }
+}
+
+var myStringStack = GenericStack<String>()
+myStringStack.push("Ali")
+myStringStack.push("Wasan")
+myStringStack.push("Khidmat")
+myStringStack.push("Joggi")
+
+print("Popping: \(myStringStack.pop())")
+print("Popping: \(myStringStack.pop())")
+print("Popping: \(myStringStack.pop())")
+print("Popping: \(myStringStack.pop())")
