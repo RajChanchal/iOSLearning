ba0ddac51ac7af29c55c0e96cbacf66bc4b137cf
Chanchal Raj <rajchanchalkhatri@gmail.com>
2025-12-16 22:10:33 +0400

    Starting SwiftUI

---
+++ 100 Days of Learning/Day 25/ColorPicker/ColorPicker/ColorPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerApp.swift
+//  ColorPicker
+//
+//  Created by Chanchal Raj on 16/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 100 Days of Learning/Day 25/ColorPicker/ColorPicker/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ColorPicker
+//
+//  Created by Chanchal Raj on 16/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("driving")
+                .resizable()
+            Text("Welcome to my app")
+                .font(.largeTitle)
+            Button("Next"){
+                
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
